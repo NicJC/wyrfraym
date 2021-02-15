5932f4687a79731afd80aca2a09cdf8c8770493c
Nic Coxen <nicjcoxen@gmail.com>
2021-02-15 22:03:29 +1300

    layout

--- themes/wyrfraym/static/js/app.js
+++
@@ -1,4 +0,0 @@
-const hero = document.querySelector('.hero');
-const slider = document.querySelector('.slider');
-const intro = document.querySelector('.intro');
-const headline = document.querySelector('.headline');
